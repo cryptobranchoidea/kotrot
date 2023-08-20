28c953aca4a6f5707a42a0bf52095eaf1c57a3fe
seipan <yamasakipann0218@gmail.com>
2023-08-21 02:09:26 +0900

    go health

--- server/pkg/web/handler/health.go
+++ server/pkg/web/handler/health.go
@@ -1,1 +1,7 @@
 package handler
+
+import "github.com/gofiber/fiber/v2"
+
+func HealthHandler(c *fiber.Ctx) error {
+	return c.SendString("OK")
+}
--- server/pkg/web/router/health.go
+++ server/pkg/web/router/health.go
@@ -1,1 +1,10 @@
 package router
+
+import (
+	"github.com/cryptobranchoidea/fisap/pkg/web/handler"
+	"github.com/gofiber/fiber/v2"
+)
+
+func HealthRouter(app *fiber.App) {
+	app.Get("/health", handler.HealthHandler)
+}
