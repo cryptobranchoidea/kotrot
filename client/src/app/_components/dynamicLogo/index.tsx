16f82a6661dc7d147a17aa81fc57adb30a608186
miso-devel <kengo279@icloud.com>
2023-08-27 03:31:02 +0900

    client:stateを切り離す

--- client/src/app/_components/dynamicLogo/index.tsx
+++ client/src/app/_components/dynamicLogo/index.tsx
@@ -1,21 +1,17 @@
 'use client';
 import { space } from '@/ui/token';
 import { Box, Flex } from '@kuma-ui/core';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
 const Atom: FC<{ row: number; column: number }> = ({ row, column }) => {
-  const [style, setStyle] = useState({ backgroundColor: 'colors.primary' });
-  const onMouseEnter = () => setStyle({ backgroundColor: 'colors.secondary.sub' });
-  const onMouseLeave = () => setStyle({ backgroundColor: 'colors.primary' });
   const isLight = (row === 4 && column !== 2) || (row === 3 && column === 2);
   return (
     <Box
       width="15px"
       height="15px"
       borderRadius="3px"
-      backgroundColor={isLight ? 'colors.secondary.sub' : style.backgroundColor}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
+      backgroundColor={`${isLight ? 'colors.secondary.sub' : 'colors.primary'}`}
+      _hover={{ backgroundColor: 'colors.secondary.sub' }}
     />
   );
 };
