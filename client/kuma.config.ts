d16d568a2ae1d4048bc04155499200e470ef79f4
miso-devel <kengo279@icloud.com>
2023-08-26 20:05:34 +0900

    client:config setting

--- client/kuma.config.ts
+++ client/kuma.config.ts
@@ -1,7 +1,8 @@
 import { createTheme } from '@kuma-ui/core';
 
 const theme = createTheme({
-  fontSizes: { sm: '12px', md: '16px', lg: '24px', xl: '60px' },
+  colors: { primary: '#638FFF', secondary: { main: '#638FFF', sub: '#FFED4D' } },
+  fontSizes: { sm: '12px', md: '16px', lg: '24px', xl: '80px' },
 });
 
 type UserTheme = typeof theme;
