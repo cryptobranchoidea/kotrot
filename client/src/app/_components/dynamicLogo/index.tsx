816bc268d70123ac102625bb655695d38d26825b
miso-devel <kengo279@icloud.com>
2023-08-27 00:38:42 +0900

    client:Logoコンポーネントの作成

--- client/src/app/_components/dynamicLogo/index.tsx
+++ client/src/app/_components/dynamicLogo/index.tsx
@@ -1,34 +1,44 @@
 'use client';
-import { Box } from '@kuma-ui/core';
-import { useState } from 'react';
+import { space } from '@/ui/token';
+import { Box, Flex } from '@kuma-ui/core';
+import { FC, useState } from 'react';
 
-export const DynamicLogo = () => {
-  const [rotate, setRotate] = useState('rotate(10deg)');
-  const onMouseEnter = () => setRotate('rotate(20deg)');
-  const onMouseLeave = () => setRotate('rotate(10deg)');
+const Atom: FC<{ row: number; column: number }> = ({ row, column }) => {
+  const [style, setStyle] = useState({ backgroundColor: 'colors.primary' });
+  const onMouseEnter = () => setStyle({ backgroundColor: 'colors.secondary.sub' });
+  const onMouseLeave = () => setStyle({ backgroundColor: 'colors.primary' });
+  const isLight = (row === 4 && column !== 2) || (row === 3 && column === 2);
   return (
-    <Box onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-      <Box
-        height="20px"
-        width="160px"
-        backgroundColor="#FFED4D"
-        transform={rotate}
-        borderRadius={10}
-        marginBottom={10}
-        borderColor="black"
-        borderWidth={2}
-        border={'solid'}
-        transition="all 0.4s ease-in"
-      />
-      <Box
-        height="80px"
-        width="160px"
-        backgroundColor="#638FFF"
-        borderRadius={10}
-        borderColor="black"
-        borderWidth={2}
-        border={'solid'}
-      />
-    </Box>
+    <Box
+      width="15px"
+      height="15px"
+      borderRadius="3px"
+      backgroundColor={isLight ? 'colors.secondary.sub' : style.backgroundColor}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    />
+  );
+};
+
+const LogoLow: FC<{ row: number; rowSize: number }> = ({ row, rowSize }) => {
+  return (
+    <Flex gap={space.sm} justifyContent="right">
+      {[...Array(rowSize)].map((_, column) => (
+        <Atom row={row} column={column} key={column} />
+      ))}
+    </Flex>
+  );
+};
+
+export const DynamicLogo: FC = () => {
+  const rowSizes = [5, 5, 5, 5, 5, 5, 6, 7];
+  return (
+    <Flex justifyContent="center">
+      <Flex flexDirection="column" gap={space.sm}>
+        {rowSizes.map((rowSize, index) => {
+          return <LogoLow row={index} rowSize={rowSize} key={index} />;
+        })}
+      </Flex>
+    </Flex>
   );
 };
