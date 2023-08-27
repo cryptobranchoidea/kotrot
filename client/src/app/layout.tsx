db01a464161d74aa5c55901ed654b9e25638c317
miso-devel <kengo279@icloud.com>
2023-08-28 01:58:45 +0900

    client:link調整

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             height="100vh"
             width={'33%'}
             margin="auto"
-            borderLeft="4px solid #638FFF"
-            borderRight="4px solid #638FFF"
+            borderLeft={['', '', '4px solid #638FFF']}
+            borderRight={['', '', '4px solid #638FFF']}
           >
             {children}
           </Flex>
--- client/src/ui/primitive/appLink/index.tsx
+++ client/src/ui/primitive/appLink/index.tsx
@@ -1,22 +1,24 @@
 import { space } from '@/ui/token';
-import { Link } from '@kuma-ui/core';
+import { Box } from '@kuma-ui/core';
+import Link from 'next/link';
 import { FC } from 'react';
+
 type TProps = { href: string; text: string };
 
 export const AppLink: FC<TProps> = ({ href, text }) => {
   return (
-    <Link
-      fontSize="fontSizes.md"
-      fontWeight="bold"
-      border="3px solid"
-      color="black"
-      padding={space.sm}
-      _hover={{ borderColor: 'colors.secondary.main', opacity: 0.8 }}
-      borderRadius={3}
-      textDecoration="none"
-      href={href}
-    >
-      {text}
+    <Link href={href} style={{ textDecoration: 'none', padding: space.sm }}>
+      <Box
+        fontSize="fontSizes.md"
+        fontWeight="bold"
+        border="3px solid"
+        color="black"
+        padding={space.sm}
+        _hover={{ borderColor: 'colors.secondary.main', opacity: 0.8 }}
+        borderRadius={3}
+      >
+        {text}
+      </Box>
     </Link>
   );
 };
