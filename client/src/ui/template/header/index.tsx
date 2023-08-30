f3cb533c872d6c7718437b09b1cb53f6b0f32753
miso-devel <kengo279@icloud.com>
2023-08-30 23:53:32 +0900

    client:header修正

--- client/src/ui/template/header/index.tsx
+++ client/src/ui/template/header/index.tsx
@@ -1,18 +1,14 @@
 'use client';
 import { AppLink } from '@/ui/primitive/appLink';
 import { Logo } from '@/ui/primitive/logo';
-import { space } from '@/ui/token';
-import { Flex, Text } from '@kuma-ui/core';
+import { Flex } from '@kuma-ui/core';
 import { FC } from 'react';
 
 export const Header: FC = () => {
   return (
     <Flex justifyContent="center">
       <AppLink href="/">
-        <Flex gap={space.lg} justifyContent="center" alignItems="center">
-          <Logo />
-          <Text as="h1" fontFamily="Londrina Solid" children="Fisap" fontSize="fontSizes.lg" />
-        </Flex>
+        <Logo />
       </AppLink>
     </Flex>
   );
---
+++ client/src/ui/template/wrapper/withHeaderWrapper/index.tsx
@@ -0,0 +1,16 @@
+import { space } from '@/ui/token';
+import { Box, Text } from '@kuma-ui/core';
+import { FC, ReactNode } from 'react';
+import { Header } from '../../header';
+
+type TProps = { children: ReactNode; title: string };
+
+export const WithHeaderWrapper: FC<TProps> = ({ children, title }) => {
+  return (
+    <Box marginY={space.xl}>
+      <Header />
+      <Text as="h2" children={title} textAlign="center" marginY={space.xl} fontSize="fontSizes.lg" />
+      {children}
+    </Box>
+  );
+};
