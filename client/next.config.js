86c7d3fe78d45e2f038caa0524da3c09cb8c7571
miso-devel <kengo279@icloud.com>
2023-09-03 05:43:30 +0900

    client:build error

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const { withKumaUI } = require('@kuma-ui/next-plugin');
-const nextConfig = {};
+const path = require('path');
+const nextConfig = {
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+};
 
 module.exports = withKumaUI(nextConfig);
