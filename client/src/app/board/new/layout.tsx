1d70738edccaf12e6941fac47ab26a7c844be90f
miso-devel <kengo279@icloud.com>
2023-09-03 04:46:16 +0900

    client:相対パスで指定

--- client/src/app/board/new/layout.tsx
+++ client/src/app/board/new/layout.tsx
@@ -1,5 +1,4 @@
-import { Header } from '@/ui/template/Header';
-import 'modern-css-reset/dist/reset.min.css';
+import { Header } from '../../../ui/template/Header';
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
   return <Header title="Board作成">{children}</Header>;
