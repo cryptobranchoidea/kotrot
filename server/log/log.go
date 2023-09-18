1005ad1c4f4a6b175ae152168a7c9ff10255717e
yamasaki <yamasakipann0218@gmail.com>
2023-09-19 01:57:06 +0900

    go momon new log

--- server/log/log.go
+++ server/log/log.go
@@ -3,15 +3,27 @@ package log
 import (
 	"os"
 
+	"github.com/labstack/gommon/log"
 	"github.com/seipan/loghook"
+
+	"github.com/cryptobranchoidea/kotrot/config"
 )
 
-func New() *loghook.Logger {
+func NewHoook() *loghook.Logger {
 	logger := loghook.NewLogger(os.Getenv("LOGIMG"), os.Getenv("LOGNAME"), "discord", os.Getenv("WEBHOOKURL"))
 	logger.SetLevel(loghook.InfoLevel)
 	if os.Getenv("ENVIRONMENT") == "prd" {
 		logger.NoSendInfo()
 	}
 	logger.SetErrorWebhook(os.Getenv("ERRORWEBHOOKURL"))
+	return logger
+}
+
+func New() *log.Logger {
+	logger := log.New("application")
+	logger.SetLevel(log.INFO)
+	if config.IsLocal() || config.IsDev() {
+		logger.SetLevel(log.DEBUG)
+	}
 	return logger
 }
